0b050c8be9b4b37a1eedfdc56676a548d1463ab1
trusause <37892656+trusause@users.noreply.github.com>
2022-06-27 23:43:49 +0800

    Update Vip4Application.java
    修改redis的配置文件

--- src/main/java/com/trusause/vip4/Vip4Application.java
+++ src/main/java/com/trusause/vip4/Vip4Application.java
@@ -17,7 +17,7 @@ public class Vip4Application {
     @Bean
     public Redisson redisson() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setDatabase(0);
+        config.useSingleServer().setAddress("redis://192.168.31.50:6379").setDatabase(0);
         return (Redisson) Redisson.create(config);
     }
 
